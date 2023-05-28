cd6f1012fa6037e72b82baec9841d0afbd076d9c
Aryan Pitliya <38190250+aryanxp@users.noreply.github.com>
2023-05-28 12:41:41 +0530

    added rubik font✅

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      display: ["Rubik"],
+    },
     extend: {
       colors: {
         primary: "#fafbff",
