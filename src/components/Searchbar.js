eece37af2e1dcc6b377f159ff4ecd1077393f028
Aryan Pitliya <38190250+aryanxp@users.noreply.github.com>
2023-05-28 12:42:25 +0530

    Changed UI of search bar🦄

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { FiSearch } from "react-icons/fi";
 
 function Searchbar() {
   return (
-    <div>Searchbar</div>
-  )
+    <div className="mt-7 p-3 bg-white w-full rounded-md shadow-sm text-slate-400 font-display flex">
+      <FiSearch className="mr-3 ml-2 mt-1 " />
+      <input
+        type="text"
+        className="bg-white rounded inline-block outline-none"
+        placeholder="Search by coin"
+      />
+    </div>
+  );
 }
 
-export default Searchbar
+export default Searchbar;
