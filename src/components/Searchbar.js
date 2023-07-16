3acb34348975ed94ee6206d4c8baa90c44b2d086
manjul-singla <manjulsingla01@gmail.com>
2023-07-17 01:44:38 +0530

    ui changes

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -3,11 +3,11 @@ import { FiSearch } from "react-icons/fi";
 
 function Searchbar() {
   return (
-    <div className="mt-7 h-11 p-3 bg-white grow rounded-md shadow-sm text-slate-400 font-display flex">
-      <FiSearch className="mr-3 ml-2 mt-1 " />
+    <div className="mt-7 items-center p-3 bg-white flex grow rounded-md shadow-sm text-slate-400 font-display">
+      <FiSearch className="mr-3 ml-2 " />
       <input
         type="text"
-        className="bg-white rounded inline-block outline-none"
+        className=" bg-white rounded inline-block outline-none"
         placeholder="Search by coin"
       />
     </div>
