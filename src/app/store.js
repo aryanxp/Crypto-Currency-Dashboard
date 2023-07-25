b4ef8815a872f4b7d0fe9828939482cda35acc45
manjul-singla <manjulsingla01@gmail.com>
2023-07-26 00:19:21 +0530

    store created & thunk added for marketcomp.+ changes

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,43 @@
-import { configureStore } from "@reduxjs/toolkit";
+// store.js
+import { configureStore, createAsyncThunk } from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
 
-export const store = configureStore({
-  reducer: {},
+const initialState = {
+  cryptoList: [],
+};
+
+export const fetchCryptoList = createAsyncThunk(
+  "marketCap/fetchCryptoList",
+  async () => {
+    try {
+      const response = await fetch(
+        "https://api.allorigins.win/raw?url=https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=1&page=1"
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch data from the API.");
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      throw new Error("Error fetching crypto list: " + error.message);
+    }
+  }
+);
+
+const marketCapReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case fetchCryptoList.fulfilled.type: // Use the action type from createAsyncThunk
+      return { ...state, cryptoList: action.payload }; // Make sure to use action.payload to access the data
+    default:
+      return state;
+  }
+};
+
+const store = configureStore({
+  reducer: {
+    marketCap: marketCapReducer,
+  },
+  middleware: [thunk], // Add Redux Thunk middleware
 });
+
+export default store;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client"; // Import createRoot from "react-dom/client"
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { store } from "./app/store";
+import store from "./app/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
